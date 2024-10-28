98de5dd442ae7fa4e0abf07ddc00312cd8306b1e
Shashwot KC <49743516+shashwot2@users.noreply.github.com>
2024-10-28 11:31:10 -0400

    Fix protected route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,29 +1,12 @@
-import React from 'react';
-import { Stack } from 'expo-router';
-import { AuthProvider, useAuth } from '../context/auth';
 
-export default function RootLayout() {
+import { Slot } from 'expo-router';
+import { AuthProvider } from '../context/auth';
+
+export default function Root() {
+  // Set up the auth context and render our layout inside of it.
   return (
     <AuthProvider>
-      <AppNavigator />
+      <Slot />
     </AuthProvider>
-  );
-}
-
-function AppNavigator() {
-  const { user } = useAuth();
-
-  return (
-    <Stack screenOptions={{ headerShown: false }}>
-      {user ? (
-        <>
-          <Stack.Screen name="(root)" />
-        </>
-      ) : 
-        <>
-          <Stack.Screen name="sign-in" />
-        </>
-}
-    </Stack>
   );
 }
