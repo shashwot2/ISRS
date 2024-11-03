78014564d39dd4751de5c4d6d1dabce3549e36e1
Weihao Li <102840013+kesfc@users.noreply.github.com>
2024-11-02 21:01:47 -0400

    some conflict error

--- app/(root)/(tabs)/deckselection.tsx
+++ app/(root)/(tabs)/deckselection.tsx
@@ -1,57 +1,3 @@
-<<<<<<< Updated upstream
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions, Pressable } from 'react-native';
-import Carousel from 'react-native-reanimated-carousel';
-
-const { width } = Dimensions.get('window');
-
-const decks = [
-  { title: 'Deck 1' },
-  { title: 'Deck 2' },
-  { title: 'Deck 3' },
-];
-
-export default function DeckSelection() {
-  return (
-    <View style={styles.carousel}>
-    <Carousel
-      width={width * 0.6}
-      height={500}
-      data={decks}
-      renderItem={({ item }) => (
-        <View style={styles.card}>
-          <Text style={styles.title}>{item.title}</Text>
-        </View>
-      )}
-      style={styles.carousel}
-      pagingEnabled={true} 
-    />
-    <Pressable onPress={() => {}}>
-        <Text>Custom Word</Text>
-    </Pressable>
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  carousel: {
-    alignSelf: 'center',
-    paddingVertical: 100,
-  },
-  card: {
-    backgroundColor: '#414143',
-    borderRadius: 15,
-    height: 200,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  title: {
-    color: 'white',
-    fontSize: 22,
-    fontWeight: 'bold',
-  },
-=======
 import React, { useState } from 'react';
 import { View, Text, StyleSheet, FlatList, Dimensions, TouchableOpacity } from 'react-native';
 import Review from '../../../components/review';  // Import the Review component
@@ -137,7 +83,6 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
         color: '#FFF',
     },
->>>>>>> Stashed changes
 });
 
 export default DeckSelection;
